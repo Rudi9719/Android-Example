6b6dcc33a6a534048831047047063acb1c578743
Gregory Rudolph <rudi@lolay.com>
2016-04-29 22:47:59 -0400

    Initial commit

---
+++ app/src/test/java/net/stein13/android_example/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package net.stein13.android_example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
